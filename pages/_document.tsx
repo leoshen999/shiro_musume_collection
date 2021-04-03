5e9ee5f4e83a265c015fb18230ceb5e53e22e535
leoshen <leoshen@cloudmosa.com>
2021-04-03 18:53:41 +0800

    Use Noto Sans JP as default font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,11 @@ export default class RootDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
