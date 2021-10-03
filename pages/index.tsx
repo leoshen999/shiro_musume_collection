2b8106d425b33dbc1e3a77b852b6ea4c4cb5f344
leoshen <leoshen@cloudmosa.com>
2021-10-03 17:55:48 +0800

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import ShiroMusumeList from "../src/components/ShiroMusumeList";
 
 export default function IndexPage() {
-  return <ShiroMusumeList />;
+  return (
+    <>
+      <Head>
+        <title>城プロRE：城娘図鑑</title>
+      </Head>
+      <ShiroMusumeList />
+    </>
+  );
 }
