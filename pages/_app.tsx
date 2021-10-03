a0ec687dd3d8bae85b8969138619e40d068a63a5
leoshen <leoshen@cloudmosa.com>
2021-10-04 00:21:43 +0800

    Fix bad styling for SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
 import "../src/index.css";
 import { GlobalContextProvider } from "../src/GlobalContext";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <GlobalContextProvider>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import Document, {
   DocumentContext,
   Html,
@@ -5,11 +6,27 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 
 export default class RootDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+
+    return {
+      ...initialProps,
+      styles: [
+        ...Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
 
   render() {
