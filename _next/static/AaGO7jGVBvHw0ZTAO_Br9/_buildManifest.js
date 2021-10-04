e6b373cad0b2b2fe1a17227985df5d0fe498a061
leoshen <leoshen@cloudmosa.com>
2021-10-04 22:30:50 +0800

    Adjust scrollbar style

--- _next/static/AaGO7jGVBvHw0ZTAO_Br9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/85-5ec307a8efd513c915ee.js","static/chunks/pages/index-aba6f50b701d5f534e61.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xONLMm_04FSA101TZHyaz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/85-5ec307a8efd513c915ee.js","static/chunks/pages/index-5c57a1b2d94068f800fc.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
