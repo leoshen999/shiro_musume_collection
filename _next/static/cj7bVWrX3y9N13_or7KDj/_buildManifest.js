e110ca24e398f5e227a29b5a565b033dfcf6e83b
leoshen <leoshen@cloudmosa.com>
2021-10-07 01:35:32 +0800

    Update to e99e24670e59254de6f03cc72dceb7535f4567b2

---
+++ _next/static/cj7bVWrX3y9N13_or7KDj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/956-d5cc1f7e2d0ebe48bf94.js","static/chunks/pages/index-7b8e5419e708b2566c78.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
