3d1718ed7325485a99822d838b4a96e33f1d12ae
leoshen <leoshen@cloudmosa.com>
2021-10-09 15:49:06 +0800

    Add an empty service worker
    To make A2HS work. Not sure whether this is enough.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,27 @@ import type { AppProps } from "next/app";
 import "../src/index.css";
 import { GlobalContextProvider } from "../src/GlobalContext";
 
+function setupServiceWorker() {
+  if (!navigator.serviceWorker) return;
+  navigator.serviceWorker.register(
+    process.env.NEXT_PUBLIC_FRONTEND_BASE + "/sw.js"
+  );
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentElement) {
       jssStyles.parentElement.removeChild(jssStyles);
+    }
+
+    if (
+      document.readyState === "interactive" ||
+      document.readyState === "complete"
+    ) {
+      setupServiceWorker();
+    } else {
+      document.addEventListener("DOMContentLoaded", setupServiceWorker, true);
     }
   }, []);
 
