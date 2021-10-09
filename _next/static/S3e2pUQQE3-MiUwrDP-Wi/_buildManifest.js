3dd749595cb2bc888066771e14c199ba2c93ec9e
leoshen <leoshen@cloudmosa.com>
2021-10-09 15:50:31 +0800

    Update to 3d1718e

--- _next/static/S3e2pUQQE3-MiUwrDP-Wi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/956-d5cc1f7e2d0ebe48bf94.js","static/chunks/pages/index-e826e28ac8540828ea4c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
