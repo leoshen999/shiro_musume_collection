7da952cfe2175237a2f823923945335b91683b85
leoshen <leoshen@cloudmosa.com>
2021-10-10 17:29:25 +0800

    Support offline mode by using service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,73 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('shiro_musume_collection').then((cache) => cache.addAll([])),
-  );
+var kAppSrcCacheNamePrefix = "shiro_musume_collection_app_src_";
+var kAppSrcCacheName = kAppSrcCacheNamePrefix + XXX_APP_SRC_TIMESTAMP_XXX;
+var kAppSrcFileList = XXX_APP_SRC_FILE_LIST_XXX;
+
+var kDynamicResCacheName = "shiro_musume_collection_dynamic_res";
+var kDynamicResDir = XXX_DYNAMIC_RES_DIR_XXX;
+
+self.addEventListener("install", (e) => {
+  // Push this service worker to activate.
+  self.skipWaiting();
+
+  // Download src of new version app.
+  var promise = (async () => {
+    var cache = await caches.open(kAppSrcCacheName);
+    await cache.addAll(kAppSrcFileList);
+  })();
+
+  e.waitUntil(promise);
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+self.addEventListener("activate", (e) => {
+  // Take control for all clients in the scope.
+  if (self.clients && clients.claim) clients.claim();
+
+  // Remove out-of-date caches.
+  var promise = (async () => {
+    var keys = await caches.keys();
+    var outOfDateKeys = keys.filter(
+      (key) =>
+        key.startsWith(kAppSrcCacheNamePrefix) && key !== kAppSrcCacheName
+    );
+
+    await Promise.all(outOfDateKeys.map((oodk) => caches.delete(oodk)));
+  })();
+
+  e.waitUntil(promise);
+});
+
+self.addEventListener("fetch", (e) => {
+  var promise = (async () => {
+    var url = new URL(e.request.url);
+    var cacheName = "";
+    if (url.href.startsWith(self.registration.scope)) {
+      if (url.pathname.startsWith(kDynamicResDir))
+        cacheName = kDynamicResCacheName;
+      else if (kAppSrcFileList.includes(url.pathname))
+        cacheName = kAppSrcCacheName;
+    }
+
+    // For non-cacheable url, fetch directly.
+    if (!cacheName) return fetch(e.request);
+
+    // Get from cache.
+    var cache = await caches.open(cacheName);
+    var cacheResp = await cache.match(e.request);
+
+    // Fetch from network and write to cache.
+    var fetchPromise = (async () => {
+      var networkResp = await fetch(e.request);
+      e.waitUntil(cache.put(e.request, networkResp.clone()));
+      return networkResp;
+    })();
+
+    // Use cache if presented. Else, use network.
+    if (cacheResp) {
+      e.waitUntil(fetchPromise);
+      return cacheResp;
+    }
+    return fetchPromise;
+  })();
+
+  e.respondWith(promise);
 });
--- tools/build.js
+++ tools/build.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var fse = require('fs-extra');
+var fs = require("fs");
+var fse = require("fs-extra");
 var child_process = require("child_process");
 var chalk = require("chalk");
 var path = require("path");
@@ -12,8 +12,8 @@ process.chdir(PROJECT_ROOT);
 
 console.log(chalk.green("Remove previous artifacts"));
 
-fs.rmdirSync(".next", {recursive: true});
-fs.rmdirSync("out", {recursive: true});
+fs.rmdirSync(".next", { recursive: true });
+fs.rmdirSync("out", { recursive: true });
 
 console.log(chalk.green("Build frontend"));
 
@@ -30,3 +30,38 @@ child_process.execSync("next export", {
 console.log(chalk.green("Copy backend files"));
 
 fse.copySync("backend", "out/backend");
+
+console.log(chalk.green("Update cache info for service worker"));
+
+var app_src_files = ["", "favicon.ico"];
+
+function addFilesToSrc(dir) {
+  var dirents = fs.readdirSync("out/" + dir, { withFileTypes: true });
+  dirents.forEach((dirent) => {
+    if (dirent.isDirectory()) addFilesToSrc(dir + "/" + dirent.name);
+    else if (dirent.isFile()) {
+      app_src_files.push(dir + "/" + dirent.name);
+    }
+  });
+}
+addFilesToSrc("_next");
+addFilesToSrc("weapon_images");
+
+var app_src_timestamp_str =
+  '"' + new Date().toISOString().replace(/[T:.Z-]/g, "") + '"';
+var app_src_file_list_str =
+  "[" +
+  app_src_files
+    .map((f) => '"' + process.env.NEXT_PUBLIC_FRONTEND_BASE + f + '"')
+    .join(",") +
+  "]";
+var dynamic_res_dir_str =
+  '"' + process.env.NEXT_PUBLIC_FRONTEND_BASE + 'musume_images/"';
+
+var sw = fs.readFileSync("out/sw.js", "utf8");
+sw = sw
+  .replace(/XXX_APP_SRC_TIMESTAMP_XXX/g, app_src_timestamp_str)
+  .replace(/XXX_APP_SRC_FILE_LIST_XXX/g, app_src_file_list_str)
+  .replace(/XXX_DYNAMIC_RES_DIR_XXX/g, dynamic_res_dir_str);
+
+fs.writeFileSync("out/sw.js", sw, "utf8");
