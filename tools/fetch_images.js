3032afc5186ffa330cb14832bce7c5b90aea47af
leoshen <leoshen@cloudmosa.com>
2021-10-10 17:29:58 +0800

    Format some scripts

--- tools/fetch_images.js
+++ tools/fetch_images.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var request = require('request');
+var fs = require("fs");
+var request = require("request");
 
 function padding_str(num, size) {
   var s = "000000000" + num.toString();
@@ -12,23 +12,27 @@ function sleep(ms) {
   });
 }
 
-var musumes = JSON.parse(fs.readFileSync(__dirname + "/../src/resources/musumes.json", "utf8"));
+var musumes = JSON.parse(
+  fs.readFileSync(__dirname + "/../src/resources/musumes.json", "utf8")
+);
 
 var target_dir = __dirname + "/../public/musume_images/";
-if (!fs.existsSync(target_dir))
-  fs.mkdirSync(target_dir);
+if (!fs.existsSync(target_dir)) fs.mkdirSync(target_dir);
 
-(async function() {
+(async function () {
   for (const musume of musumes) {
     const target_path = target_dir + padding_str(musume.id, 4) + ".png";
     if (fs.existsSync(target_path)) continue;
 
-    process.stdout.write("Fetch " + padding_str(musume.id, 4) + " " + musume.name + " ... ");
+    process.stdout.write(
+      "Fetch " + padding_str(musume.id, 4) + " " + musume.name + " ... "
+    );
     const url =
-      "https://image.swiki.jp/img0/?host=scre&page=%E7%94%BB%E5%83%8F%E3%81%A7%E4%B8%80%E8%A6%A7RE&src=" + encodeURIComponent(musume.name) + ".png";
+      "https://image.swiki.jp/img0/?host=scre&page=%E7%94%BB%E5%83%8F%E3%81%A7%E4%B8%80%E8%A6%A7RE&src=" +
+      encodeURIComponent(musume.name) +
+      ".png";
     request(url).pipe(fs.createWriteStream(target_path));
     process.stdout.write("done\n");
     await sleep(3 * 1000);
   }
 })();
-
